38bb05e588dccdcc7ba9098226c307964b387a51
ELDSC <sarangojosue6@gmail.com>
2026-04-06 21:59:52 -0500

    Añadida seccion de Catalogo, UI de ProductCard y mock de productos

---
+++ src/components/data/products.ts
@@ -0,0 +1,35 @@
+export interface Product {
+  id:        number;
+  name:      string;
+  image:     string;
+  price:     number;
+  oldPrice:  number;
+  badge?:    string;    
+  featured?: boolean;   
+}
+
+export const featuredProducts: Product[] = [
+  {
+    id:       1,
+    name:     'Gold Stand Whey Protein',
+    image:    'https://cloudinary.images-iherb.com/image/upload/f_auto,q_auto:eco/images/opn/opn02414/l/53.jpg',
+    price:    44.99,
+    oldPrice: 59.99,
+  },
+  {
+    id:       2,
+    name:     'Creatina Monohidratada',
+    image:    'https://gymbroperu.com/wp-content/uploads/2025/02/CREATINA-MONOHIDRATADA-ESSENTIALS-DRAGON-PHARMA.png',
+    price:    24.99,
+    oldPrice: 34.99,
+    badge:    'TOP VENTAS',
+    featured: true,
+  },
+  {
+    id:       3,
+    name:     'Shaker Pro Steel',
+    image:    'https://http2.mlstatic.com/D_NQ_NP_643088-MPE106686552602_022026-O.webp',
+    price:    12.99,
+    oldPrice: 19.99,
+  },
+];
